82e1c3adb3658425745a79028dce0109476e4a7e
wishtoday238 <wishtoday238@gmail.com>
2025-09-28 00:41:30 +0800

    修复保存潜影盒时可能得数组越界错误

--- src/main/java/org/wishtoday/rto/raidToOldVersion/Event/impl/PlayerListener.java
+++ src/main/java/org/wishtoday/rto/raidToOldVersion/Event/impl/PlayerListener.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.wishtoday.rto.raidToOldVersion.Network.CarpetHelloPacket;
 
 public class PlayerListener implements Listener {
     @SuppressWarnings("unused")
